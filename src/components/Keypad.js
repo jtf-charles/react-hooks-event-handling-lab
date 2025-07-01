4b9261e9a636eaf21368f0c0684b8e97718e6c08
unknown <jtf-charles>
2025-07-01 13:44:57 -0500

    Completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function DispayText(){
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={DispayText}></input>
+        </div>
     )
 }
 
